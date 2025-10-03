cd03a79804768433ab36e73e323aeef3b95e71d8
prakharqa7@gmail.com <prakharqa9@gmail.com>
2025-10-03 19:05:08 -0400

    updated demo test page as per Prakhar TL review comments

--- src/test/java/com/qa/opencart/tests/DemoPageTest.java
+++ src/test/java/com/qa/opencart/tests/DemoPageTest.java
@@ -5,9 +5,16 @@ import org.openqa.selenium.By;
 public class DemoPageTest {
 	
 	By demo = By.id("demo");
+	By cart = By.id("cart");
 	
 	public void getDemo() {
 		System.out.println("demo");
+		System.out.println("demo details");
+	}
+	
+	public void addTocart() {
+		System.out.println("cart");
+		System.out.println("cart details");
 	}
 	
 }
