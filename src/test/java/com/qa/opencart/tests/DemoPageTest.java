08108e9ba76c14ef3475873292b34dadfd035019
prakharqa7@gmail.com <prakharqa9@gmail.com>
2025-10-03 18:45:30 -0400

    added demo page

---
+++ src/test/java/com/qa/opencart/tests/DemoPageTest.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.tests;
+
+import org.openqa.selenium.By;
+
+public class DemoPageTest {
+	
+	By demo = By.id("demo");
+	
+	public void getDemo() {
+		System.out.println("demo");
+	}
+	
+}
