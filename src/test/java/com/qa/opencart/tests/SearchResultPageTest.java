f041829388a9e731535ca7d0fa76f0784ff4f7e4
prakharqa7@gmail.com <prakharqa9@gmail.com>
2025-10-03 22:47:46 -0400

    added search page test setup method

--- src/test/java/com/qa/opencart/tests/SearchResultPageTest.java
+++ src/test/java/com/qa/opencart/tests/SearchResultPageTest.java
@@ -1,7 +1,18 @@
 package com.qa.opencart.tests;
 
+import org.testng.annotations.BeforeClass;
+
 import com.qa.opencart.base.BaseTest;
 
 public class SearchResultPageTest extends BaseTest{
-	
+	@BeforeClass
+	public void searchPageSetUp() {
+		//accPage = loginPage.doLogin(prop.getProperty("username"), System.getProperty("password"));
+		String pwd = System.getProperty("password");
+		if (pwd != null) {
+			accPage = loginPage.doLogin(prop.getProperty("username"), System.getProperty("password"));
+		} else {
+			accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+		}
+	}
 }
